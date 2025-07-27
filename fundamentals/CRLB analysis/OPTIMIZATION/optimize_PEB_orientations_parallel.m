ac0ca2444f24b041561cc4696d2e337727a58150
kevin-acuna <kjacuna.inf@gmail.com>
2025-07-27 18:13:36 +0200

    optimizacion con K=3

--- fundamentals/CRLB analysis/OPTIMIZATION/optimize_PEB_orientations_parallel.m
+++ fundamentals/CRLB analysis/OPTIMIZATION/optimize_PEB_orientations_parallel.m
@@ -13,7 +13,7 @@ rng('default');
 
 % ======================== CONFIGURATION ========================
 
-K_orientations = [3,4]; % Number of LED orientations to optimize
+K_orientations = [4,5,6,7]; % Number of LED orientations to optimize
 system_params.optimization_metric = 'rms';     % 'mean', 'max', 'rms', 'percentile_90'
 L = 3; W = 3; 
 Hmax = 1.2; step = 0.2;
@@ -297,7 +297,7 @@ for k_idx = 1:length(K_orientations)
     diary off;
     
     % Clean up for next iteration
-    %close all;
+    close all;
     pause(1); % Brief pause to ensure proper cleanup
 end
 
