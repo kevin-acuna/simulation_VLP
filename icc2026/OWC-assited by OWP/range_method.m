0200f8add83dd934691ed5929fc87bf1d5996af1
kevin-acuna <kjacuna.inf@gmail.com>
2025-09-30 01:08:47 +0200

    estimaciones

---
+++ icc2026/OWC-assited by OWP/range_method.m
@@ -0,0 +1,64 @@
+clear all, close all, clc
+
+% Parámetros
+z = 0.76;      % altura del receptor
+H = 2;        % altura transmisor
+h = H - z;    % diferencia vertical
+
+tilt = 30;                % inclinación [deg]
+azimuth_1 = 0;            % azimut [deg]
+azimuth_2 = 90;            % azimut [deg]
+
+n_r = [0,0,1]';           % normal receptor (mirando al techo)
+n_t = [0,0,-1; ...
+       sind(tilt)*cosd(azimuth_1), sind(tilt)*sind(azimuth_1), -cosd(tilt);...
+       sind(tilt)*cosd(azimuth_2), sind(tilt)*sind(azimuth_2), -cosd(tilt)]'; % 3x2
+
+T = [0,0,H]';             % posición del Tx
+
+% Grilla 2D
+dx = 0.05;
+Rx = -2:dx:2;
+Ry = -2:dx:2;
+[XX,YY] = meshgrid(Rx,Ry);
+NN = numel(XX);
+R = [XX(:)'; YY(:)'; z*ones(1,NN)];  % puntos del receptor
+
+d      = R - T;
+d_norm = sqrt(sum(d.^2,1));
+d_unit = d ./ d_norm;
+
+cos_phi = n_t' * d_unit;        % 2xN (emisión para ambos modos)
+phi     = acosd(cos_phi);        % 2xN
+
+phi_c = phi<=40;
+R_nt1 = R(:,phi_c(1,:)==1);
+R_nt2 = R(:,phi_c(2,:)==1);
+R_nt3 = R(:,phi_c(3,:)==1);
+R_filter = R(:,sum(phi_c,1)==3);
+length(R_filter)
+
+
+    % Rangos en X e Y
+    x_min = min(R_filter(1,:));  x_max = max(R_filter(1,:)); x_max-x_min
+    y_min = min(R_filter(2,:));  y_max = max(R_filter(2,:)); y_max-y_min
+    z0    = R_filter(3,1);       % misma Z que los puntos filtrados (todos iguales)
+
+    % ---------- (A) Rectángulo mínimo alineado a ejes ----------
+    Xr = [x_min, x_max, x_max, x_min, x_min];
+    Yr = [y_min, y_min, y_max, y_max, y_min];
+    Zr = z0 * ones(1,5);
+
+
+
+
+figure(1)
+hold on
+plot3(R_nt1(1,:),R_nt1(2,:),R_nt1(3,:),'o','LineWidth',1)
+plot3(R_nt2(1,:),R_nt2(2,:),R_nt2(3,:),'o','LineWidth',1)
+plot3(R_nt3(1,:),R_nt3(2,:),R_nt3(3,:),'o','LineWidth',1)
+plot3(R_filter(1,:),R_filter(2,:),R_filter(3,:),'o','LineWidth',1)
+plot3(0,0,0.76,'o','LineWidth',2,'Color','k')
+plot3(Xr, Yr, Zr, '-','LineWidth',2,'Color','k')
+axis([-2 2 -2 2 0 2])
+grid minor
--- icc2026/OWC-assited by OWP/untitled.m
+++ icc2026/OWC-assited by OWP/untitled.m
@@ -1,6 +1,46 @@
 clc, clear all, close all
 
-H=2;
-h=0.75;
-dH = H - h;
+% from the datasheet
+datasheet = csvread('pattern_smoothed_3.csv',1,0);
+
+
+% estimacion por lambertianos
+phi = 0:0.5:90;
+lamb =cosd(phi).^1.5;
+
+
+figure(1)
+hold on
+plot(datasheet(:,1), datasheet(:,2)/100,'LineWidth',3,'DisplayName','Datasheet')
+plot(phi,lamb)
+legend
+grid minor
+
+
+%%
+% estimation with polynomios
+p1 =[-2.87071219e-05,2.04429106e-03,-6.27359812e-02,1.85501661e-01,9.94769800e01]/100; %0 - 46
+p2 =[5.41209126e-04,-8.38922902e-02,4.37043922,-8.24163885e01,3.30525716e02]/100; %46 a 58
+p3 =[-1.1142e-04,3.3874e-02,-3.3845e+00,1.1325e+02]/100; % 58 a 90
+
+phi_range = -90:0.5:90;
+i_est = [];
+for phi = phi_range
+    phi=abs(phi);
+    if phi <= 46
+        i_rel = polyval(p1, phi);
+    elseif phi <= 58
+        i_rel = polyval(p2, phi);    
+    elseif phi <= 90
+        i_rel = polyval(p3, phi);
+    else 
+        i_rel = 0;
+    end
+    i_est = [i_est i_rel];
+end
+
+plot(phi_range,i_est, 'LineWidth',4,'DisplayName','Polinomial','LineStyle',':')
+xline(0,  'r--', 'x=0');
+xline(46, 'r--', 'x=46');
+xline(58, 'r--', 'x=58');
 
