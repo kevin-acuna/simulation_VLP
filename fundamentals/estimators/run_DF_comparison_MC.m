36aa68eb9019552de8a5eaa0c111f44a1e723bab
kevin-acuna <kjacuna.inf@gmail.com>
2026-04-29 01:18:27 +0200

    resultados para MC 1000 en DF

--- fundamentals/estimators/run_DF_comparison_MC.m
+++ fundamentals/estimators/run_DF_comparison_MC.m
@@ -13,7 +13,7 @@ addpath('../core');
 % =================================================
 rng(42);
 TEST_MODE = false;       % 'true' for fast grid, 'false' for full paper grid
-M_trials = 100;         % Monte Carlo trials per position
+M_trials = 1000;         % Monte Carlo trials per position
 N_or = 5;               % Number of orientations
 save_files = 1;         
 receiver_mode = 'fixed';
